de4376f3daa1ff97b83e6bb66f8244bd03fff6af
quyenUit <nguyenquyenboss@gmail.com>
2022-10-06 23:33:22 +0700

    api

--- index.js
+++ index.js
@@ -10,17 +10,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 
-app.use(cors());
+// app.use(cors());
 // app.get("/", (req, res, next) => {
 //   res.render("index");
 // });
 
 //
-//require("./app/routes/users.routes")(app);
+require("./app/routes/users.routes")(app);
 
 //Node.js Login System mit Express, JWT & MySQL (Rest API)
-const router = require("./app/routes/router");
-app.use("/api", router);
+// const router = require("./app/routes/router");
+// app.use("/api", router);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
